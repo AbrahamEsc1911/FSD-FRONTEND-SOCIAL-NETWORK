41078f9fcf9c0f8250f5818d9f8106312bddac14
AEscobarAngola <abrancho1908@gmail.com>
2024-08-01 19:43:29 +0200

    feat: body view stablished

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Footer } from './components/Footer/Footer'
+import { Body } from './views/Body/Body'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Body />
       <Footer />
     </>
   )
