61ba5793128f42aa52b82f7d8daf3e66e6d0cde6
AEscobarAngola <abrancho1908@gmail.com>
2024-08-02 17:59:53 +0200

    feat: adding api call type post to login a user, and adding functions to login view

--- src/Services/auth.services.js
+++ src/Services/auth.services.js
@@ -2,14 +2,29 @@ const URL = "http://localhost:4000/api/v1/users"
 
 export const register = async (credentials) => {
     try {
-        console.log(contentType)
-        console.log(credentials)
         const response = await fetch(`${URL}/register`, {
             method: "POST",
             headers: {
                 "Content-type": "application/json"
             },
             body: JSON.stringify(credentials)
+        })
+
+        return await response.json()
+
+    } catch (error) {
+        console.error(`Error fetching data ${error}`)
+    }
+}
+
+export const login = async (credenitials) => {
+    try {
+        const response = await fetch(`${URL}/login`, {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(credenitials)
         })
 
         return await response.json()
--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -1,23 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { CInputs } from '../../components/CInputs/CInputs'
+import './Login.css'
+import { useNavigate } from 'react-router-dom'
+import { login } from '../../Services/auth.services'
 
 export const Login = () => {
 
-    const handleChangeLog = () => {
+    const [credentials, setCredentials] = useState(
+        {
+            email: "",
+            password: ""
+        }
+    )
 
+    const [warningMessage, setWarningMessage] = useState(false)
+    const [invalidAccesMessage, setInvalidAccesMessage] = useState(false)
+    const [passwordChart, setPasswordChart] = useState(false)
+    const navigate = useNavigate()
+
+    const handleChangeLog = (e) => {
+        setCredentials(prevState => (
+            {
+                ...prevState,
+                [e.target.name]: e.target.value
+            }
+        ))
     }
 
-    const loginButton = () => {
-        
+    const loginButton = async () => {
+        if (credentials.email.length === 0 || credentials.password.length === 0) {
+            setWarningMessage(true)
+            setInvalidAccesMessage(false)
+        } else if (credentials.password.length < 8 || credentials.password.length > 12) {
+            setWarningMessage(false)
+            setPasswordChart(true)
+            setInvalidAccesMessage(false)
+        } else {
+            const response = await login(credentials)
+            setPasswordChart(false)
+            if (response.success) {
+                navigate("/profile")
+            } else {
+                setInvalidAccesMessage(true)
+            }
+        }
     }
 
-  return (
-    <>
-    <h3>Ingresar</h3>
-    <CInputs type="email" name="email" placeholder="Email" onChange={handleChangeLog} />
-    <CInputs type="password" name="password" placeholder="Email" onChange={handleChangeLog} />
-    <CInputs type="button" name="login" value="ingresar" onClick={loginButton} />
+    return (
+        <>
+            <h3>Ingresar</h3>
+            <CInputs type="email" name="email" placeholder="Email" onChange={handleChangeLog} />
+            <CInputs type="password" name="password" placeholder="Constraseña" onChange={handleChangeLog} />
+            <p className={warningMessage ? "" : "hidden-content"}>Email y contraseña son requeridos</p>
+            <p className={passwordChart ? "" : "hidden-content"}>la contraseña debe ser mayor a 8 y menor a 12 caracteres</p>
+            <p className={invalidAccesMessage ? "" : "hidden-content"}>Correo o contraseña incorrectos</p>
+            <CInputs type="button" name="login" value="ingresar" onClick={loginButton} />
 
-    </>
-  )
+        </>
+    )
 }
