2e8d5452d99ce16b222634df342f2787f13f4b99
AEscobarAngola <abrancho1908@gmail.com>
2024-08-09 12:59:49 +0200

    feat: new single post view created, brings all post data

--- src/views/SiglePost/SiglePost.jsx
+++ src/views/SiglePost/SiglePost.jsx
@@ -1,29 +1,70 @@
 import React, { useEffect, useState } from 'react'
-import { getPostById } from '../../Services/posts.services'
-import { useParams } from 'react-router-dom'
+import { getPostById, likeDislike } from '../../Services/posts.services'
+import { useNavigate, useParams } from 'react-router-dom'
+import { CInputs } from '../../components/CInputs/CInputs'
 
 export const SiglePost = () => {
 
-    const {id} = useParams()
+    const passport = JSON.parse(localStorage.getItem("passport"))
+    let token = null
+    if (passport) { token = passport.token }
+
+    const { id } = useParams()
     const [post, setPost] = useState(
         {
-
+            comments: [],
+            createdAt: "",
+            likes: [],
+            post_message: "",
+            user: {},
+            _id: ""
         }
     )
+    const navigate = useNavigate()
 
     useEffect(() => {
-     
+
         const bringPostById = async () => {
             const res = await getPostById(id)
-            console.log(res)
+            setPost(res.data)
         };
 
-         bringPostById()
+        bringPostById()
 
-    }, )
-    
+    }, [])
 
-  return (
-    <div>SiglePost</div>
-  )
+    const likeThisPosts = async (e) => {
+        const postId = e.target.name
+        if(token){
+            await likeDislike(token, postId)
+            const res = await getPostById(id)
+            setPost(res.data)
+        } else {
+            navigate('/login')
+        }
+    }
+
+    return (
+        <>
+            <p>{post.user.name}</p>
+            <p>{post.post_message}</p>
+            <p>{post.createdAt}</p>
+            <p>likes: {post.likes.length}</p>
+            <p>comentarios: {post.comments.length}</p>
+            <CInputs type="button" value="like" name={post._id} onClick={likeThisPosts} />
+            <div> Comentarios:
+                {post.comments.map((comments) => {
+                    return (
+                        <div key={comments._id}>
+                            <div>{comments.user.name}</div>
+                            <div><img src={comments.user.profile} alt="user-profile" /></div>
+                           <div>{comments.message}</div>
+                           <div>{comments.createdAt}</div>
+                        </div>
+                    )
+                })
+                }
+            </div>
+        </>
+    )
 }
