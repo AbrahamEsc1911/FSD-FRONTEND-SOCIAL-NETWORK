928e98c98b2ebdd75c1336ec32113cacb7827075
AEscobarAngola <abrancho1908@gmail.com>
2024-08-11 14:29:05 +0200

    feat: new component created, block for add content in it, it has css style for ui

---
+++ src/components/CBlockContent/CBlockContent.jsx
@@ -0,0 +1,14 @@
+import './CBlockContent.css'
+
+import React from 'react'
+
+export const CBlockContent = () => {
+  return (
+    <>
+    <div className='ui-main-block'>
+
+    </div>
+
+    </>
+  )
+}
