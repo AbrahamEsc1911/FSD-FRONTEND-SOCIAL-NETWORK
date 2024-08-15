859c10f095ed1b7df57844a7e052abef0366cf8b
AEscobarAngola <abrancho1908@gmail.com>
2024-08-15 16:34:06 +0200

    update: adding some corrections and ajustmensts on new post component

--- src/components/CNewPost/CNewPost.jsx
+++ src/components/CNewPost/CNewPost.jsx
@@ -1,27 +1,60 @@
-import React from 'react'
-import './CNewPost.css'
+import React from "react";
+import "./CNewPost.css";
 
-export const CNewPost = ({userName, profile, buttonName, onChange, onClick, inputName }) => {
+export const CNewPost = ({
+  userName,
+  profile,
+  buttonName,
+  onChange,
+  onClick,
+  inputName,
+  clasNameforErrorMessage,
+  clasNameForEmtyMessage,
+  onClickClose,
+}) => {
   return (
     <>
-    <div className="new-post-section">
-            <div className="new-post-close">
-              <img src={profile} alt="close-icon" className="close-icon"/>
-            </div>
-            <div className="new-post-header">
-              <div className="new-post-profile"></div>
-              <div className="new-post-name">
-                <h4 className="text-no-margin">{userName}</h4>
-                <p className="text-no-margin">Public</p>
-              </div>
-            </div>
-            <div>
-              <textarea name={inputName} id="new-post-message" placeholder="New Post" maxLength={280} onChange={onChange}></textarea>
-            </div>
-            <div className="new-post-button">
-              <input type="button" value="Send" name={buttonName} className="new-post-send-button" onClick={onClick}/>
-            </div>
+      <div className="new-post-section">
+        <div className="new-post-close">
+          <img
+            src="./images/close.png"
+            alt="close-icon"
+            className="close-icon"
+            onClick={onClickClose}
+          />
+        </div>
+        <div className="new-post-header">
+          <div className="new-post-profile">
+            <img src={profile} alt="close-icon" />
           </div>
+          <div className="new-post-name">
+            <h4 className="text-no-margin">{userName}</h4>
+            <p className="text-no-margin">Public</p>
+          </div>
+        </div>
+        <div>
+          <textarea
+            name={inputName}
+            id="new-post-message"
+            placeholder="What are you thinking"
+            maxLength={280}
+            onChange={onChange}
+          ></textarea>
+        </div>
+        <div className={clasNameforErrorMessage}>
+          Error creating a new posts, try again later
+        </div>
+        <div className={clasNameForEmtyMessage}>new post coulnt be empty</div>
+        <div className="new-post-button">
+          <input
+            type="button"
+            value="Send"
+            name={buttonName}
+            className="new-post-send-button"
+            onClick={onClick}
+          />
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
