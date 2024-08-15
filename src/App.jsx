dea72e79dfe53345aa1f37a8e8f954e3b3f21077
AEscobarAngola <abrancho1908@gmail.com>
2024-08-15 17:11:57 +0200

    feat: allowing to show some elements on app if have or not passport

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,43 @@
-import { useState } from 'react'
-import './App.css'
-import { Footer } from './components/Footer/Footer'
-import { Body } from './views/Body/Body'
-import { NavBar } from './views/NavBar/NavBar'
+import { useState } from "react";
+import "./App.css";
+import { Footer } from "./components/Footer/Footer";
+import { Body } from "./views/Body/Body";
+import { NavBar } from "./views/NavBar/NavBar";
 
 function App() {
+  const passport = JSON.parse(localStorage.getItem("passport"));
+  let token = null;
+  if (passport) {
+    token = passport.token;
+  }
 
   return (
     <>
-      <div className="app-container">
-        <div className="navbar-container">
-          <NavBar />
-        </div>
-        <div className="content-container">
-          <Body />
-          <Footer />
-        </div>
+      <div>
+        {passport && (
+          <div className="app-container">
+            <div className="navbar-container">
+              <NavBar />
+            </div>
+            <div className="content-container">
+              <Body />
+              <Footer />
+            </div>
+          </div>
+        )}
+      </div>
+      <div>
+        {!passport && (
+          <div className="content-container-full-width">
+            <div>
+              <Body />
+            </div>
+            <Footer />
+          </div>
+        )}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
