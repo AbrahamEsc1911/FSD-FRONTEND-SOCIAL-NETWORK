c562f72e45744ffb538c669d131bfc2a362ab2ac
AEscobarAngola <abrancho1908@gmail.com>
2024-08-21 10:13:52 +0200

    feat: adding a new element an text on component users suggested to follow

--- src/components/CRecomendationBlock/CRecomendationBlock.jsx
+++ src/components/CRecomendationBlock/CRecomendationBlock.jsx
@@ -11,8 +11,11 @@ export const CRecomendationBlock = ({profile, userName, buttonName, buttonOnClic
         <div className="recomendation-content">
           <div className="text-name-navigation">
           <h4 className="text-no-margin" id={userProfile} onClick={() => onClickToGoUserProfile(userProfile)}>{userName}</h4>
+          <p className="small-font-size text-no-margin">Ciudad</p>
           </div>
-          <input type="button" value="follow" name={buttonName} onClick={buttonOnClick} className='follow-button-recomendation'/>
+        </div>
+        <div>
+        <input type="button" value="" name={buttonName} onClick={buttonOnClick} className='follow-button-recomendation'/>
         </div>
       </div>
     </>
